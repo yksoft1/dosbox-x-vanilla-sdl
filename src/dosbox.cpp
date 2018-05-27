3e8b71de2745d8ff9ff3f9dfa612055883b3eec4
yksoft1 <yksoft1@gmail.com>
2018-05-27 23:40:08 +0800

    Add preference to enable PC-98 SOUND BIOS (currently the MSW bit only).

--- src/dosbox.cpp
+++ src/dosbox.cpp
@@ -1174,6 +1174,10 @@ void DOSBOX_SetupConfigSections(void) {
 	Phex = secprop->Add_hex("pc-98 fm board io port", Property::Changeable::WhenIdle,0);
 	Phex->Set_help("If set, helps to determine the base I/O port of the FM board. A setting of zero means to auto-determine the port number.");
 
+	Pbool = secprop->Add_bool("pc-98 fm board bios",Property::Changeable::WhenIdle,false);
+	Pbool->Set_help("Set FM board BIOS enabled bit in MEMSW 4 for some games that require it.\n"
+					"TODO: Real emulation of FM board BIOS");
+	
 	Pbool = secprop->Add_bool("pc-98 buffer page flip",Property::Changeable::WhenIdle,false);
 	Pbool->Set_help("If set, the game's request to page flip will be delayed to vertical retrace, which can eliminate tearline artifacts.\n"
                     "Note that this is NOT the behavior of actual hardware. This option is provided for the user's preference.");
