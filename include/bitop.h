ce94eeaebd8504bddae37354209f62e48215c11e
yksoft1 <yksoft1@gmail.com>
2018-05-31 20:09:32 +0800

    More bitop.(joncampbell123)

--- include/bitop.h
+++ include/bitop.h
@@ -3,31 +3,103 @@
 
 #if __cplusplus < 201103L
 #define constexpr
-#define static_assert(a,...)
+#define static_assert(a,...) assert(a)
 #endif
 
 namespace bitop {
 
+/* Return the number of bits of the data type.
+ *
+ * @return Number of bits in data type T
+ */
 template <typename T> static inline constexpr unsigned int type_bits(void) {
     return (unsigned int)sizeof(T) * 8u;
 }
 
+/* Return data type T with all bits 0
+ *
+ * @return Type T with zero bits
+ */
 template <typename T> static inline constexpr T allzero(void) {
     return (T)0;
 }
 
+/* Return data type T with all bits 1
+ *
+ * @return Type T with set bits
+ */
 template <typename T> static inline constexpr T allones(void) {
     return (T)( ~((T)0) );
 }
 
+/* private */
 template <typename T> static inline constexpr unsigned int _bitlength_recursion(const T v,const unsigned int bits) {
-    return (v != 0) ? _bitlength_recursion(v >> (T)1u,bits + 1u) : bits;
+	return (v != allzero<T>()) ? _bitlength_recursion(v >> (T)1u,bits + 1u) : bits;
 }
 
-template <typename T> static inline constexpr unsigned int bitlength(const T v) {
-    return _bitlength_recursion(v,0);
+/* Return minimum number of bits required to represent value 'v' in data type 'T'
+ *
+ *  @return Number of bits needed
+ */
+template <typename T, const T v> static inline constexpr unsigned int bitlength(void) {
+	return _bitlength_recursion<T>(v,0);
 }
 
+template <const unsigned int v> static inline constexpr unsigned int bitlength(void) {
+	return bitlength<unsigned int,v>();
+}
+
+template <typename T> static inline unsigned int bitlength(T v) {
+	unsigned int c = 0;
+	while ((v & 0xFFUL) == 0xFFUL) {
+		v >>= (T)8UL;
+		c += (T)8;
+	}
+	while (v != allzero<T>()) {
+		v >>= (T)1UL;
+		c++;
+	}
+	return c;
+}
+
+/* Return number of sequential 1 bits counting from LSB in value 'v' of type 'T'
+*
+* @return Number of bits
+*/
+template <typename T> static inline constexpr unsigned int _bitseqlength_recursionlsb(const T v,const unsigned int bits) {
+	return (v & 1u) ? _bitseqlength_recursionlsb<T>(v >> (T)1u,bits + 1u) : bits;
+}
+
+template <typename T,const T v> static inline constexpr unsigned int bitseqlengthlsb(void) {
+	return _bitseqlength_recursionlsb<T>(v,0);
+}
+
+template <const unsigned int v> static inline constexpr unsigned int bitseqlengthlsb(void) {
+	return bitseqlengthlsb<unsigned int,v>();
+}
+
+template <typename T> static inline void _bitseqlengthlsb_1(unsigned int &c,T &v) {
+	while ((v & 0xFFUL) == 0xFFUL) {
+		v >>= (T)8UL;
+		c += (T)8;
+	}
+	while (v & 1u) {
+		v >>= (T)1UL;
+		c++;
+	}
+}
+
+template <typename T> static inline unsigned int bitseqlengthlsb(T v) {
+	unsigned int c = 0;
+	_bitseqlengthlsb_1(/*&*/c,/*&*/v);
+	return c;
+}
+
+
+/* Return binary mask of bit 'a'
+ *
+ * @return Bitmask
+ */
 template <unsigned int a,typename T> static inline constexpr T bit2mask(void) {
     static_assert(a < type_bits<T>(), "bit2mask constexpr bit count too large for data type");
     return (T)1U << (T)a;
@@ -37,11 +109,19 @@ template <typename T> static inline constexpr T bit2mask(unsigned int a) {
     return (T)1U << (T)a;
 }
 
+/* Return binary mask of MSB in type 'T'
+ *
+ * @return Bitmask
+ */
 template <typename T> static inline constexpr T type_msb_mask(void) {
     return bit2mask<T>(type_bits<T>() - 1u);
 }
 
 #if __cplusplus >= 201103L //There seemed to be no way to implement this without constexpr
+/* Return binary mask of 'a' LSB bits starting at bit offset 'offset' in type 'T'
+ *
+ * @return Bitmask
+ */
 template <const unsigned int a,const unsigned int offset,typename T=unsigned int> static inline constexpr T bitcount2masklsb(void) {
 	/* NTS: special case for a == type_bits because shifting the size of a register OR LARGER is undefined.
 	 * On Intel x86 processors, with 32-bit integers, x >> 32 == x >> 0 because only the low 5 bits are used
@@ -57,6 +137,8 @@ template <const unsigned int a,const unsigned int offset,typename T=unsigned int
 template <const unsigned int a,typename T> static inline constexpr T bitcount2masklsb(void) {
     return bitcount2masklsb<a,0u,T>();
 }
+
+
 #endif
 
 template <typename T> static inline constexpr T bitcount2masklsb(const unsigned int a,const unsigned int offset=0) {
@@ -66,7 +148,31 @@ template <typename T> static inline constexpr T bitcount2masklsb(const unsigned 
 	 return ((a < type_bits<T>()) ? (bit2mask<T>(a) - (T)1u) : allones<T>()) << (T)offset;
 }
 
+/* Return binary mask of 'a' MSB bits starting at bit offset 'offset' in type 'T'
+ *
+ * @return Bitmask
+ */
+template <const unsigned int a,const unsigned int offset,typename T> static inline constexpr T bitcount2maskmsb(void) {
+	/* NTS: special case for a == type_bits because shifting the size of a register OR LARGER is undefined.
+	 * On Intel x86 processors, with 32-bit integers, x >> 32 == x >> 0 because only the low 5 bits are used
+	 * a % type_bits<T>() is there to keep a < type_bits<T> in case your C++11 compiler likes to trigger
+	 * all static_assert<> clauses even if the ternary would not choose that path. */
+	static_assert(a <= type_bits<T>(), "bitcount2maskmsb constexpr bit count too large for data type");
+	static_assert(offset < type_bits<T>(), "bitcount2maskmsb constexpr offset count too large for data type");
+	static_assert((a+offset) <= type_bits<T>(), "bitcount2maskmsb constexpr bit count + offset count too large for data type");
+	return ((a != (T)0) ? ((T)(allones<T>() << (T)(type_bits<T>() - a)) >> (T)offset) : allzero<T>());
+}
+
+template <const unsigned int a,typename T> static inline constexpr T bitcount2maskmsb(void) {
+	return bitcount2maskmsb<a,0u,T>();
+}
+
+template <typename T> static inline constexpr T bitcount2maskmsb(const unsigned int a,const unsigned int offset=0) {
+	/* NTS: special case for a == type_bits because shifting the size of a register OR LARGER is undefined.
+	 * On Intel x86 processors, with 32-bit integers, x >> 32 == x >> 0 because only the low 5 bits are used */
+	return ((a != (T)0) ? ((T)(allones<T>() << (T)(type_bits<T>() - a)) >> (T)offset) : allzero<T>());
+}
+
 void self_test();
 
 }
-
--- src/gui/bitop.cpp
+++ src/gui/bitop.cpp
@@ -7,6 +7,7 @@ namespace bitop {
 
 void self_test(void) {
     // DEBUG
+#if __cplusplus >= 201103L
     static_assert(bitcount2masklsb<0u>() == 0u);
     static_assert(bitcount2masklsb<1u>() == 1u, "whoops");
     static_assert(bitcount2masklsb<2u>() == 3u, "whoops");
@@ -15,6 +16,7 @@ void self_test(void) {
     static_assert(bitcount2masklsb<2u,0u,uint8_t>() == 3u, "whoops");
     static_assert(bitcount2masklsb<2u,1u,uint8_t>() == 6u, "whoops");
     static_assert(bitcount2masklsb<type_bits<>()>() == allones(), "whoops");
+#endif
     assert(allones<uint32_t>() == (uint32_t)0xFFFFFFFFUL);
     assert(allzero<uint32_t>() == (uint32_t)0);
     assert(allones<uint64_t>() == (uint64_t)0xFFFFFFFFFFFFFFFFULL);
@@ -37,11 +39,93 @@ void self_test(void) {
     assert(bitlength(4u) == 3u);
     assert(bitlength(7u) == 3u);
     assert(bitlength(8u) == 4u);
+	assert(bitlength(255u) == 8u);
+	assert(bitlength(256u) == 9u);
+	assert(bitlength(512u) == 10u);
+	assert(bitlength(1024u) == 11u);
+	assert(bitlength(32767u) == 15u);
+	assert(bitlength(32768u) == 16u);
+	
     assert(bitlength(~0u) == type_bits<unsigned int>());
     assert(type_msb_mask<uint8_t>() == 0x80u);
     assert(type_msb_mask<uint16_t>() == 0x8000u);
     assert(type_msb_mask<uint32_t>() == 0x80000000UL);
     assert(type_msb_mask<uint64_t>() == 0x8000000000000000ULL);
+	
+	static_assert((bitseqlengthlsb<unsigned int, 0u>() == 0u), "whoops"); // 0
+	static_assert((bitseqlengthlsb<unsigned int, 1u>() == 1u), "whoops"); // 1
+	static_assert((bitseqlengthlsb<unsigned int, 2u>() == 0u), "whoops"); // 10
+	static_assert((bitseqlengthlsb<unsigned int, 3u>() == 2u), "whoops"); // 11
+	static_assert((bitseqlengthlsb<unsigned int, 4u>() == 0u), "whoops"); // 100
+	static_assert((bitseqlengthlsb<unsigned int, 5u>() == 1u), "whoops"); // 101
+	static_assert((bitseqlengthlsb<unsigned int, 6u>() == 0u), "whoops"); // 110
+	static_assert((bitseqlengthlsb<unsigned int, 7u>() == 3u), "whoops"); // 111
+	static_assert((bitseqlengthlsb<unsigned int, 8u>() == 0u), "whoops"); // 1000
+	static_assert((bitseqlengthlsb<unsigned int, 9u>() == 1u), "whoops"); // 1001
+	static_assert((bitseqlengthlsb<unsigned int, 10u>() == 0u), "whoops"); // 1010
+	static_assert((bitseqlengthlsb<unsigned int, 11u>() == 2u), "whoops"); // 1011
+	static_assert((bitseqlengthlsb<unsigned int, 12u>() == 0u), "whoops"); // 1100
+	static_assert((bitseqlengthlsb<unsigned int, 15u>() == 4u), "whoops"); // 1111
+	static_assert((bitseqlengthlsb<unsigned int, 23u>() == 3u), "whoops"); // 10111
+	static_assert((bitseqlengthlsb<unsigned int, 31u>() == 5u), "whoops"); // 11111
+	static_assert((bitseqlengthlsb<unsigned int, ~0u>() == type_bits<unsigned int>()), "whoops");
+	
+	assert(bitlength(0u) == 0u);
+	assert(bitlength(1u) == 1u);
+	assert(bitlength(2u) == 2u);
+	assert(bitlength(3u) == 2u);
+	assert(bitlength(4u) == 3u);
+	assert(bitlength(7u) == 3u);
+	assert(bitlength(255u) == 8u);
+	assert(bitlength(256u) == 9u);
+	assert(bitlength(512u) == 10u);
+	assert(bitlength(1024u) == 11u);
+	assert(bitlength(32767u) == 15u);
+	assert(bitlength(32768u) == 16u);
+	
+	assert(bitseqlengthlsb(0u) == 0u);
+	assert(bitseqlengthlsb(1u) == 1u);
+	assert(bitseqlengthlsb(2u) == 0u);
+	assert(bitseqlengthlsb(3u) == 2u);
+	assert(bitseqlengthlsb(4u) == 0u);
+	assert(bitseqlengthlsb(5u) == 1u);
+	assert(bitseqlengthlsb(6u) == 0u);
+	assert(bitseqlengthlsb(7u) == 3u);
+	assert(bitseqlengthlsb(255u) == 8u);
+	assert(bitseqlengthlsb(256u) == 0u);
+	assert(bitseqlengthlsb(512u) == 0u);
+	assert(bitseqlengthlsb(1024u) == 0u);
+	assert(bitseqlengthlsb(32767u) == 15u);
+	assert(bitseqlengthlsb(32768u) == 0u);
+	
+	static_assert((bitcount2maskmsb<0u,unsigned int>() == 0u), "whoops");
+	static_assert((bitcount2maskmsb<1u,unsigned int>() == (1u << (type_bits<unsigned int>() - 1u))), "whoops");
+	static_assert((bitcount2maskmsb<2u,unsigned int>() == (3u << (type_bits<unsigned int>() - 2u))), "whoops");
+	static_assert((bitcount2maskmsb<2u,1u,unsigned int>() == (3u << (type_bits<unsigned int>() - 3u))), "whoops");
+	static_assert((bitcount2maskmsb<2u,uint8_t>() == (3u << 6u)), "whoops");
+	static_assert((bitcount2maskmsb<2u,0u,uint8_t>() == (3u << 6u)), "whoops");
+	static_assert((bitcount2maskmsb<2u,1u,uint8_t>() == (3u << 5u)), "whoops");
+#if __cplusplus >= 201103L
+	static_assert(bitcount2maskmsb<type_bits()>() == allones(), "whoops");
+#endif	
+	
+	assert(bitcount2masklsb<unsigned int>(0u) == 0u);
+	assert(bitcount2masklsb<unsigned int>(1u) == 1u);
+	assert(bitcount2masklsb<unsigned int>(2u) == 3u);
+	assert(bitcount2masklsb<unsigned int>(2u,1u) == 6u);
+	assert(bitcount2masklsb<uint8_t>(2u) == 3u);
+	assert(bitcount2masklsb<uint8_t>(2u,0u) == 3u);
+	assert(bitcount2masklsb<uint8_t>(2u,1u) == 6u);
+	assert(bitcount2masklsb<unsigned int>(type_bits<unsigned int>()) == allones<unsigned int>());
+	
+	assert(bitcount2maskmsb<unsigned int>(0u) == 0u);
+	assert(bitcount2maskmsb<unsigned int>(1u) == (1u << (type_bits<unsigned int>() - 1u)));
+	assert(bitcount2maskmsb<unsigned int>(2u) == (3u << (type_bits<unsigned int>() - 2u)));
+	assert(bitcount2maskmsb<unsigned int>(2u,1u) == (3u << (type_bits<unsigned int>() - 3u)));
+	assert(bitcount2maskmsb<uint8_t>(2u) == (3u << 6u));
+	assert(bitcount2maskmsb<uint8_t>(2u,0u) == (3u << 6u));
+	assert(bitcount2maskmsb<uint8_t>(2u,1u) == (3u << 5u));
+	assert(bitcount2maskmsb<unsigned int>(type_bits<unsigned int>()) == allones<unsigned int>());	
 }
 
 }
