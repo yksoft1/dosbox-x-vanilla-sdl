d76bf0871e60a5a2334703bc5fa5d8349af0ddc0
yksoft1 <yksoft1@gmail.com>
2018-06-20 21:53:35 +0800

    Add basic support for booting from BIOS image(joncampbell123).

--- src/dos/dos_programs.cpp
+++ src/dos/dos_programs.cpp
@@ -705,6 +705,8 @@ private:
 public:
    
 	void Run(void) {
+		std::string bios;
+		bool bios_boot = false;
         bool swaponedrive = false;
 		bool force = false;
 		
@@ -717,6 +719,9 @@ public:
 
         if (cmd->FindExist("-force",true))
             force = true;
+			
+		if (cmd->FindString("-bios",bios,true))
+			bios_boot = true;
 
 		//Hack To allow long commandlines
 		ChangeToLongCmd();
@@ -727,6 +732,49 @@ public:
 			return;
 		}
 		
+		if (bios_boot) {
+			Bit32u isz1,isz2;
+			
+			if (bios.empty()) {
+				WriteOut("Must specify BIOS image to boot\n");
+				return;
+			}
+			
+			// NOTES:
+			//
+			// Regarding PC-98 mode, you should use an older BIOS image.
+			// The PC-9821 ROM image(s) I have appear to rely on bank
+			// switching parts of itself to boot up and operate.
+			//
+			// Update: I found some PC-9801 ROM BIOS images online, which
+			// ALSO seem to have a BIOS.ROM, ITF.ROM, etc...
+			//
+			// So, this command will not be able to run those
+			// images until port 43Dh (the I/O port used for
+			// bank switching) is implemented in DOSBox-X.
+			//
+			// In IBM PC/AT mode, this should hopefully allow using old
+			// 386/486 BIOSes in DOSBox-X.
+			
+			/* load it */
+			FILE *romfp = getFSFile(bios.c_str(), &isz1, &isz2);
+			if (romfp == NULL) {
+				WriteOut("Unable to open BIOS image\n");
+				return;
+			}
+			Bitu loadsz = (isz2 + 0xFU) & (~0xFU);
+			if (loadsz == 0) loadsz = 0x10;
+			if (loadsz > (IS_PC98_ARCH ? 0x18000 : 0x20000)) loadsz = (IS_PC98_ARCH ? 0x18000 : 0x20000);
+			Bitu segbase = 0x100000 - loadsz;
+			LOG_MSG("Loading BIOS image %s to 0x%lx, 0x%lx bytes",bios.c_str(),(unsigned long)segbase,(unsigned long)loadsz);
+			fseek(romfp, 0, SEEK_SET);
+			fread(GetMemBase()+segbase,loadsz,1,romfp);
+			fclose(romfp);
+
+			/* boot it */
+			throw int(8);
+		}
+
 		bool bootbyDrive=false;
 		FILE *usefile_1=NULL;
 		FILE *usefile_2=NULL;
