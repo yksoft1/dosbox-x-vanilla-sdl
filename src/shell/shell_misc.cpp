f4c8d7beaa5cd3e5c171c50a9b98c207df9f5501
yksoft1 <yksoft1@gmail.com>
2018-06-24 12:04:32 +0800

    Complete ESC line clearing fix.

--- src/shell/shell_misc.cpp
+++ src/shell/shell_misc.cpp
@@ -547,12 +547,17 @@ void DOS_Shell::InputCommand(char * line) {
 				//
 				// DOSBox / DOSBox-X have always acted as if DOSKEY is loaded in a fashion, so
 				// we'll emulate DOSKEY behavior here.
-				while (str_len > 0) {
+				
+				while (str_index < str_len) {
+					outc(' ');
+					str_index++;
+				}
+				while (str_index > 0) {
 					outc(8);
 					outc(' ');
 					outc(8);
 					MoveCaretBackwards();
-					str_len--;
+					str_index--;
 				}
 				
 				*line = 0; // reset the line.
