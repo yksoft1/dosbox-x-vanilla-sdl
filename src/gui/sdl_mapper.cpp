f6abef312cf5fe23641917b99225cf523f6985f0
yksoft1 <yksoft1@gmail.com>
2018-11-05 08:18:44 +0800

    Fix SDL2 mapper button redraw (joncampbell123).

--- src/gui/sdl_mapper.cpp
+++ src/gui/sdl_mapper.cpp
@@ -2458,8 +2458,10 @@ extern void GFX_UpdateDisplayDimensions(int width, int height);
 #endif
 
 static void DrawButtons(void) {
+#if defined(C_SDL2)
+	SDL_FillRect(mapper.draw_surface,0,0);
+#else
 	SDL_FillRect(mapper.surface,0,0);
-#if !defined(C_SDL2)
 	SDL_LockSurface(mapper.surface);
 #endif
 	for (CButton_it but_it = buttons.begin();but_it!=buttons.end();but_it++) {
@@ -3656,7 +3658,7 @@ void MAPPER_RunInternal() {
 			DrawButtons();
         } else {
 #if defined(C_SDL2)
-            SDL_UpdateWindowSurface(mapper.window);
+            //SDL_UpdateWindowSurface(mapper.window);
 #endif
         }
 		BIND_MappingEvents();
