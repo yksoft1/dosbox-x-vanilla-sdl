75cc1ee5bb5fc287df38ddda8e2ff48f3362a2cf
yksoft1 <yksoft1@gmail.com>
2018-11-10 22:45:48 +0800

    Add PC-98 text scroll RAM emulation (joncampbell123)

--- src/ints/bios.cpp
+++ src/ints/bios.cpp
@@ -2382,12 +2382,40 @@ extern bool                         enable_pc98_188usermod;
 extern bool                         pc98_31khz_mode;
 extern bool                         pc98_attr4_graphic;
 
+extern unsigned char			pc98_text_first_row_scanline_start;  /* port 70h */
+extern unsigned char			pc98_text_first_row_scanline_end; 	 /* port 72h */
+extern unsigned char 			pc98_text_row_scanline_blank_at;	 /* port 74h */
+extern unsigned char          	pc98_text_row_scroll_lines;          /* port 76h */
+extern unsigned char            pc98_text_row_scroll_count_start;    /* port 78h */
+extern unsigned char            pc98_text_row_scroll_num_lines;      /* port 7Ah */
+
 void pc98_update_text_layer_lineheight_from_bda(void) {
     unsigned char c = mem_readb(0x53C);
     unsigned char lineheight = mem_readb(0x53B) + 1;
 
     pc98_gdc[GDC_MASTER].force_fifo_complete();
     pc98_gdc[GDC_MASTER].row_height = lineheight;
+
+	if (lineheight > 16) { // usually 20
+		pc98_text_first_row_scanline_start = 0x1E;
+		pc98_text_first_row_scanline_end = lineheight - 3;
+		pc98_text_row_scanline_blank_at = 16;
+	}
+	else {
+		pc98_text_first_row_scanline_start = 0;
+		pc98_text_first_row_scanline_end = lineheight - 1;
+		pc98_text_row_scanline_blank_at = lineheight;
+	}
+	
+    pc98_text_row_scroll_lines = 0;
+    pc98_text_row_scroll_count_start = 0;
+    pc98_text_row_scroll_num_lines = 0;	
+	
+	vga.crtc.cursor_start = 0;
+	vga.draw.cursor.sline = 0;
+	
+	vga.crtc.cursor_end = lineheight - 1;
+	vga.draw.cursor.eline = lineheight - 1;
 }
 
 void pc98_update_text_lineheight_from_bda(void) {
