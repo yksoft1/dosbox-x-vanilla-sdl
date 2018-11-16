0a60f377b0ad934df8a4c233632e469d9e8ca31c
yksoft1 <yksoft1@gmail.com>
2018-11-16 11:09:31 +0800

    Remove PC-98 Auto EOI hack (joncampbell123).

--- src/dosbox.cpp
+++ src/dosbox.cpp
@@ -1324,12 +1324,6 @@ void DOSBOX_SetupConfigSections(void) {
 	Pbool = secprop->Add_bool("dma page registers write-only",Property::Changeable::WhenIdle,false);
 	Pbool->Set_help("Normally (on AT hardware) the DMA page registers are read/write. Set this option if you want to emulate PC/XT hardware where the page registers are write-only.");
 
-	Pbool = secprop->Add_bool("pc-98 auto eoi master",Property::Changeable::WhenIdle,true);
-    Pbool->Set_help("If set, and running in PC-98 mode, the master PIC is programmed to run in auto EOI mode");
-
-    Pbool = secprop->Add_bool("pc-98 auto eoi slave",Property::Changeable::WhenIdle,true);
-    Pbool->Set_help("If set, and running in PC-98 mode, the slave PIC is programmed to run in auto EOI mode");
-	 
 	Pbool = secprop->Add_bool("enable slave pic",Property::Changeable::WhenIdle,true);
 	Pbool->Set_help("Enable slave PIC (IRQ 8-15). Set this to 0 if you want to emulate a PC/XT type arrangement with IRQ 0-7 and no IRQ 2 cascade.");
 
