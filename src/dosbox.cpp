59f55cb5832fcdaa2a53ddd254c23faadab00087
yksoft1 <yksoft1@gmail.com>
2019-04-08 13:58:19 +0800

    Make MPU-401 I/O configurable (joncampbell123).

--- src/dosbox.cpp
+++ src/dosbox.cpp
@@ -866,6 +866,12 @@ void DOSBOX_SetupConfigSections(void) {
 	const char* joydeadzone[] = { "0.26", 0 };
 	const char* joyresponse[] = { "1.0", 0 };
 	const char* iosgus[] = { "240", "220", "260", "280", "2a0", "2c0", "2e0", "300", "210", "230", "250", 0 };
+	const char* mpubases[] = {
+			"0",                                                                                    /* Auto */
+			"300", "310", "320", "330", "332", "334", "336", "340", "360",                          /* IBM PC */
+			"c0d0","c8d0","d0d0","d8d0","e0d0","e8d0","f0d0","f8d0",                                /* NEC PC-98 MPU98 */
+			"80d2","80d4","80d6","80d8","80da","80dc","80de",                                       /* NEC PC-98 SB16 */
+			0 };
 	const char* ios[] = {
 		"220", "240", "260", "280", "2a0", "2c0", "2e0", "300",     /* IBM PC      (base+port i.e. 220h base, 22Ch is DSP) */
 		"d2",  "d4",  "d6",  "d8",  "da",  "dc",  "de",             /* NEC PC-98   (base+(port << 8) i.e. 00D2h base, 2CD2h is DSP) */
@@ -1751,6 +1757,15 @@ void DOSBOX_SetupConfigSections(void) {
 	Pstring->Set_values(mputypes);
 	Pstring->Set_help("Type of MPU-401 to emulate.");
 
+	Phex = secprop->Add_hex("mpubase",Property::Changeable::WhenIdle,0/*default*/);
+	Phex->Set_values(mpubases);
+	Phex->Set_help("The IO address of the MPU-401.\n"
+				"Set to 0 to use a default I/O address.\n"
+				"300h to 330h are for use with IBM PC mode.\n"
+				"C0D0h to F8D0h (in steps of 800h) are for use with NEC PC-98 mode (MPU98).\n"
+				"80D2h through 80DEh are for use with NEC PC-98 Sound Blaster 16 MPU-401 emulation.\n"
+				"If not assigned (0), 330h is the default for IBM PC and E0D0h is the default for PC-98.");
+	
 	Pstring = secprop->Add_string("mididevice",Property::Changeable::WhenIdle,"default");
 	Pstring->Set_values(devices);
 	Pstring->Set_help("Device that will receive the MIDI data from MPU-401.");
