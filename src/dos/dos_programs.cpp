68b0acb1bbbaeb79cc0215d297d0c8cee06747d3
yksoft1 <yksoft1@gmail.com>
2019-09-24 15:01:46 +0800

    Don't disable XMS/EMS/HMA at BOOTing. sdlmain.cpp handled that. (joncampbell123)

--- src/dos/dos_programs.cpp
+++ src/dos/dos_programs.cpp
@@ -776,17 +776,6 @@ private:
 		WriteOut(MSG_Get("PROGRAM_BOOT_PRINT_ERROR"));
 	}
 
-	void disable_umb_ems_xms(void) {
-		Section* dos_sec = control->GetSection("dos");
-		char test[20];
-		strcpy(test,"umb=false");
-		dos_sec->HandleInputline(test);
-		strcpy(test,"xms=false");
-		dos_sec->HandleInputline(test);
-		strcpy(test,"ems=false");
-		dos_sec->HandleInputline(test);
-	}
-
 public:
    
 	void Run(void) {
@@ -1212,7 +1201,6 @@ public:
 					}
 				}
 
-				disable_umb_ems_xms();
 				void PreparePCJRCartRom(void);
 				PreparePCJRCartRom();
 
@@ -1327,8 +1315,6 @@ public:
 				WriteOut_NoParsing("PROGRAM_BOOT_UNABLE");
 				return;
 			}
-
-			disable_umb_ems_xms();
 
 			WriteOut(MSG_Get("PROGRAM_BOOT_BOOT"), drive);
 
