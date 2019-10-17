ba86a23c3a5e51010d556a0e43d4bf4bac8547aa
yksoft1 <yksoft1@gmail.com>
2019-10-18 01:14:24 +0800

    Fixes on PEGC behavior for Alone in the Dark (joncampbell123).

--- src/dos/dos_programs.cpp
+++ src/dos/dos_programs.cpp
@@ -780,6 +780,7 @@ public:
    
 	void Run(void) {
 		std::string bios;
+		bool pc98_640x200 = true;
 		bool bios_boot = false;
         bool swaponedrive = false;
 		bool force = false;
@@ -799,6 +800,12 @@ public:
 			
 		if (cmd->FindString("-bios",bios,true))
 			bios_boot = true;
+
+		// debugging options
+ 		if (cmd->FindExist("-pc98-640x200",true))
+ 			pc98_640x200 = true;
+ 		if (cmd->FindExist("-pc98-640x400",true))
+ 			pc98_640x200 = false;
 
 		/* In secure mode don't allow people to boot stuff. 
 		 * They might try to corrupt the data on it */
@@ -1378,10 +1385,11 @@ public:
                 reg_eax = 0x30;
                 reg_edx = 0x1;
 
-				/* Guess: If the boot sector is smaller than 512 bytes/sector, the PC-98 BIOS
-				 * probably sets the graphics layer to 640x200. Some games (Ys) do not
-				 * set but assume instead that is the mode of the graphics layer */
-				if (pc98_sect128) {
+				/* It seems 640x200 8-color digital mode is the state of the graphics hardware when the
+ 				 * BIOS boots the OS, and some games like Ys II assume the hardware is in this state.
+   
+                 * If I am wrong, you can pass --pc98-640x400 as a command line option to disable this. */
+ 				if (pc98_640x200) {
 					reg_eax = 0x4200; // setup 640x200 graphics
 					reg_ecx = 0x8000; // lower
 					CALLBACK_RunRealInt(0x18);
